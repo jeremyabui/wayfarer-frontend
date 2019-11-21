407eed216afd60cf2aff147e1037388a259afa5f
FelipeCosta <fac.10@outlook.com>
2019-11-21 15:32:58 -0800

    cities pages created

---
+++ src/components/Cities/Cities.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Cities extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Citiessssss</h1>
+      </section>
+    );
+  }
+}
+// const Cities = () => {
+//   return (
+//     <section className="home">
+//       <h1>Cities</h1>
+//     </section>
+//   );
+// };
+
+export default Cities;
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from '../components/Home/Home';
-import Register from '../components/Navbar/Modal/Auth/Register';
-import Login from '../components/Navbar/Modal/Auth/Login';
-import ProfileContainer from '../containers/ProfileContainer';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "../components/Home/Home";
+import Register from "../components/Navbar/Modal/Auth/Register";
+import Login from "../components/Navbar/Modal/Auth/Login";
+import ProfileContainer from "../containers/ProfileContainer";
+import Cities from "../components/Cities/Cities";
 
-export default ({currentUser, setCurrentUser}) => (
+export default ({ currentUser, setCurrentUser }) => (
   <Switch>
-    <Route exact path='/' component={Home} />
-    <Route path='/register' component={Register} />
-    <Route path='/login' render={ () => <Login currentUser={currentUser} setCurrentUser={setCurrentUser} />} />
-    <Route path='/register' component={Register} />
-    <Route path='/login' render={() => <Login currentUser={currentUser} setCurrentUser={setCurrentUser} />} />
-    <Route path='/profile' component={ProfileContainer} />
+    <Route exact path="/" component={Home} />
+    <Route path="/register" component={Register} />
+    <Route
+      path="/login"
+      render={() => (
+        <Login currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      )}
+    />
+    <Route path="/register" component={Register} />
+    <Route
+      path="/login"
+      render={() => (
+        <Login currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      )}
+    />
+    <Route path="/profile" component={ProfileContainer} />
+    <Route path="/cities" component={Cities} />
   </Switch>
 );
