c563003f70e752a4aa491203a6cc51c112e4bd1f
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-11-21 10:09:43 -0800

    working in the modal

--- src/components/Navbar/Modal/Modal.js
+++ src/components/Navbar/Modal/Modal.js
@@ -1,34 +1,34 @@
-// import React from 'react';
-// import Register from './Auth/Register';
-// // import Login from './Auth/Login';
+import React from 'react';
+import Register from './Auth/Register';
+// import Login from './Auth/Login';
 
-// class Modal extends React.Component {
-//   state = {
-//     registerClicked: false,
-//     loginClicked: false,
-//   };
-
-//   render() {
-//     return (
-//       <>
-//       <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//         <div className="modal-dialog" role="document">
-//           <div className="modal-content">
-//             <div className="modal-header">
-//               <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-//               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-//                 <span aria-hidden="true">&times;</span>
-//               </button>
-//             </div>
-//             <div className="modal-body"></div>
-//               {/* <Register registerClicked={this.state.registerClicked} /> */}
-//             </div>
-//         </div>
-//       </div>
-//       </>
-//     )
-//   }
-// }
+class Modal extends React.Component {
+  state = {
+    showModal: false, 
+  };
 
 
-// export default Modal;
+  render() {
+    return (
+      <>
+      <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body"></div>
+              {/* <Register registerClicked={this.state.registerClicked} /> */}
+            </div>
+        </div>
+      </div>
+      </>
+    )
+  }
+}
+
+
+export default Modal;
