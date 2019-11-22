e7f04306d0558169f13ee672e0a77d046ce199a9
FelipeCosta <fac.10@outlook.com>
2019-11-22 13:37:38 -0800

    fixed bugs

--- src/containers/CitiesContainer.js
+++ src/containers/CitiesContainer.js
@@ -1,23 +1,28 @@
 import React from "react";
-
 import City from "../components/Cities/City/City";
-// import api from "../components/Cities/cities.json";
-
+import api from "../components/Cities/cities.json";
 class CitiesContainer extends React.Component {
   state = {
-    loaded: false,
-    cities: []
+    cities: api
   };
 
   displayCities = cities => {
     return cities.map(city => {
-      return <City key={city._id} cityData={city} />;
+      return (
+        <div className="city-container">
+          <City cityData={city} />
+        </div>
+      );
     });
   };
 
   render() {
-    return <div>{this.displayCities(this.props.cities)}</div>;
+    return (
+      <section>
+        <h1>Citiessssss</h1>
+        <div>{this.displayCities(this.state.cities)}</div>
+      </section>
+    );
   }
 }
-
 export default CitiesContainer;
