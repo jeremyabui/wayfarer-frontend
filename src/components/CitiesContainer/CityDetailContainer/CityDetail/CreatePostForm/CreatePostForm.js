0156eea06782eab9c6c15b75cc63e748e4d9425a
jaymark <jaykmark@gmail.com>
2019-11-23 17:24:43 -0800

    Post create form is v cool.

--- src/components/CitiesContainer/CityDetailContainer/CityDetail/CreatePostForm/CreatePostForm.js
+++ src/components/CitiesContainer/CityDetailContainer/CityDetail/CreatePostForm/CreatePostForm.js
@@ -1,13 +1,88 @@
 import React from 'react';
+import axios from 'axios';
 
 class CreatePostForm extends React.Component {
   state = {
+    author: localStorage.getItem('uid'),
+    city: '',
+    title: '',
+    body: '',
+  };
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    axios.post(`${process.env.REACT_APP_API_URL}/posts/newpost`, this.state, { withCredentials: true })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  }
+
+  componentDidMount() {
+      this.setState({
+        city: this.props.cityDetails._id
+      })
+    }
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.cityDetails !== this.props.cityDetails) {
+      this.setState({
+        city: this.props.cityDetails._id
+      })
+    }
   }
   
   render() {
+    console.log(this.props.cityDetails);
     return(
       <>
+        <button className="nav-item" type="button"
+        id="register" className="nav-link" data-toggle="modal" data-target="#createPostForm" to='/register'>Create Post</button>
+        <div className="modal fade" id="createPostForm" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Register</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+              </button>
+              </div>
+              <div className="modal-body"></div>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="title">Title</label>
+                    <input
+                      onChange={this.handleChange}
+                      className="form-control form-control-lg"
+                      type="text"
+                      id="title"
+                      name="title"
+                      value={this.state.title}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="body">Body</label>
+                    <input
+                      onChange={this.handleChange}
+                      className="form-control form-control-lg"
+                      type="text"
+                      id="body"
+                      name="body"
+                      value={this.state.body}
+                    />
+                    </div>
+                  <button className="btn btn-primary float-right" type="submit">
+                  Create Post
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
