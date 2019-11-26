6bb8dcbf4770b7a3399e796b119a5512a2c700ee
jaymark <jaykmark@gmail.com>
2019-11-25 18:15:24 -0800

    sets home page default to san francisco.

--- src/components/CitiesContainer/CityDetailContainer/CityDetailContainer.js
+++ src/components/CitiesContainer/CityDetailContainer/CityDetailContainer.js
@@ -12,6 +12,18 @@ class CityDetailContainer extends React.Component {
     posts: [],
   };
 
+  componentDidMount() {
+    axios.get(`${process.env.REACT_APP_API_URL}/cities/5dd838c8bf657372e1ce0e12`, {
+      withCredentials: true
+    })
+    .then((res) => {
+      this.setState({
+        cityDetails: res.data.data,
+        loaded: true,
+      })
+    })
+  };
+
   componentDidUpdate(prevProps) {
     if (prevProps.selectedCity !== this.props.selectedCity) {
       axios.get(`${process.env.REACT_APP_API_URL}/cities/${this.props.selectedCity}`, {
